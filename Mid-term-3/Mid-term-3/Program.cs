cac6be6759ba2a7b96abc96fd1cb0a9097bcf0c9
pasawud <78560211+pasawud@users.noreply.github.com>
2021-10-03 04:12:46 +0700

    Add files via upload

---
+++ Mid-term-3/Mid-term-3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flowershop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decide = "y";
+            string selectFlower;
+
+            FlowerStore flowerStore = new FlowerStore();
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("---------------------------------");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Select number for buy flower : ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("- = - = - = - = - = - = - = - = -");
+                foreach (string i in flowerStore.flowerList)
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write((flowerStore.flowerList.IndexOf(i) + 1) + " ");
+                    Console.WriteLine("You select : {0}", i);
+                }
+                Console.WriteLine("---------------------------------");
+                Console.Write("I select number ");
+                selectFlower = Console.ReadLine();
+                Console.WriteLine("---------------------------------");
+                switch (selectFlower)
+                {
+                    case "1":
+                        flowerStore.addToCart(flowerStore.flowerList[0]);
+                        Console.WriteLine("Added" + flowerStore.flowerList[0]);
+                        break;
+                    case "2":
+                        flowerStore.addToCart(flowerStore.flowerList[0]);
+                        Console.WriteLine("Added" + flowerStore.flowerList[0]);
+                        break;
+                    default:
+                        Console.WriteLine("Added" + flowerStore.flowerList[0]);
+                        break;
+                }
+
+                Console.WriteLine("You can stop this progress? exit for >> exit << progress and press any key for continue");
+                decide = Console.ReadLine();
+                if (decide == "exit")
+                {
+                    Console.Write("Current my cart : ");
+                    flowerStore.showCart();
+                }
+
+            }
+            while (decide != "exit");
+
+        }
+        class FlowerStore
+        {
+            public List<string> flowerList = new List<string>(); List<string> cart = new List<string>();
+            public FlowerStore()
+            {
+                flowerList.Add("Rose");
+                flowerList.Add("Lotus");
+            }
+            public void addToCart(string name)
+            {
+                cart.Add(name);
+            }
+            public void showCart()
+            {
+                if (cart.Count == 0)
+                {
+                    Console.WriteLine("Cart is empty");
+                }
+                else
+                {
+                    Console.WriteLine("My Cart :"); foreach (string i in cart)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ mid-term-1/mid-term-1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hang_man
+{
+    class random
+    {
+        public int result, numword;
+        public string word;
+        char[] useingword;
+
+        public random()
+        {
+            string[] words = new string[3];
+            words[0] = "tennis";
+            words[1] = "football";
+            words[2] = "badminton";
+            //random Question 
+            Random random = new Random();
+            result = random.Next(0, 3);
+            //random
+            word = words[result];
+            //length of word 
+            numword = word.Length;
+            //change string to char arry
+            useingword = word.ToCharArray();
+        }
+    }
+    class Game
+    {
+        public Game()
+        {
+            int correct = 0;
+            int incorrect = 0;
+            string Newguess;
+
+            Console.WriteLine(" Play Game Hangman ");
+            Console.WriteLine(" =-=-=-=-=-=--=-=-=");
+            Console.WriteLine("Incorrect Score: {0} ", correct);
+
+
+            random randoms = new random();
+            char[] guess = new char[randoms.numword];
+            for (int i = 0; i < randoms.numword; i++)
+            {
+                guess[i] = '_';
+                Console.Write("{0}", guess[i]);
+            }
+            Console.Write("\ninput alphabet : ");
+            do
+            {
+                char charecter = char.Parse(Console.ReadLine());
+                for (int j = 0; j < randoms.numword; j++)
+                {
+                    if (charecter == randoms.word[j])
+                    {
+                        guess[j] = charecter;
+                    }
+                    else if (charecter != randoms.word[j])
+                    {
+                        correct = correct - 1;
+                    }
+                }
+                //score 
+                if (correct == randoms.numword)
+                {
+                    incorrect = incorrect + 1;
+                    correct = 0;
+                }
+                if (correct == randoms.numword - 1)
+                {
+                    correct = 0;
+                }
+                correct = 0;
+
+                Console.Clear();
+                Console.WriteLine(" Play Game Hangman ");
+                Console.WriteLine(" =-=-=-=-=-=--=-=-=");
+                Console.WriteLine("Incorrect Score: {0} ", incorrect);
+
+                Console.WriteLine(guess);
+                Console.WriteLine("input alphabet : ");
+                Newguess = new string(guess);
+            }
+            while (true && (incorrect < 6 && Newguess != randoms.word));
+            {
+                if (incorrect == 6)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You lose");
+                }
+                if (incorrect < 6);
+                {
+
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Hangman Game");
+                Console.WriteLine("1. Play game ");
+                Console.WriteLine("2. Exit");
+                menu = Console.ReadLine();
+            }
+            while (menu != "1" && menu != "2");
+            {
+                if(menu == "1")
+                {
+                    Console.Clear();
+                    Game game = new Game();
+                }
+                else if (menu =="2")
+                {
+                    Console.WriteLine(" ");
+                }    
+            }
+        }
+    }
+}
